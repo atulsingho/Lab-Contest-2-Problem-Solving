31c0a02491d189f33fa1850a2ef7edb608bf1682
atulsingho <atulsingho457@gmail.com>
2025-12-12 19:54:57 +0600

    Refactor interval checking logic in E-E.c

---
+++ E-E.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+   
+    double X;
+    if (scanf("%lf", &X) != 1) {
+        return 1;
+    }
+
+    if (X >= 0.0 && X <= 25.0) {
+        printf("Intervalo [0,25]\n");
+    } 
+  
+    else if (X > 25.0 && X <= 50.0) {
+        printf("Intervalo (25,50]\n");
+    } 
+    
+    else if (X > 50.0 && X <= 75.0) {
+        printf("Intervalo (50,75]\n");
+    } 
+   
+    else if (X > 75.0 && X <= 100.0) {
+        printf("Intervalo (75,100]\n");
+    } 
+  
+    else {
+        printf("Fora de intervalo\n");
+    }
+
+    return 0;
+}
