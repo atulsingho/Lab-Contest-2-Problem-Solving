75eab1475c0f7ac65c22f2f3cea592669b3e3019
atulsingho <atulsingho457@gmail.com>
2025-12-12 19:44:51 +0600

    Remove comments and clean up code formatting

---
+++ B-B.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main() {
+
+    long long A, B, C;
+    char S, Q;
+
+   
+    if (scanf("%lld %c %lld %c %lld", &A, &S, &B, &Q, &C) != 5) {
+        return 1;
+    }
+
+    long long R = 0;
+    switch (S) {
+        case '+':
+            R = A + B;
+            break;
+        case '-':
+            R = A - B;
+            break;
+        case '*':
+            R = A * B;
+            break;
+        default:
+            return 1; 
+    }
+
+  
+    if (C == R) {
+       
+        printf("Yes\n");
+    } else {
+        
+        printf("%lld\n", R);
+    }
+
+    return 0;
+}
